c3c82d7ff73e80a4e3a4d4ce3d77c48c5e2735ce
kuzminkdk <kuzminkdk@gmail.com>
2026-06-08 19:58:25 +0300

    Change commands.sh

--- ccip/send-tokens/commands.sh
+++ ccip/send-tokens/commands.sh
@@ -3,7 +3,7 @@
 # — Scripts —
 
 # Deploy contract
-forge script script/DeployTokenTransferor.s.sol:DeployTokenTransferor --broadcast --rpc-url avalanche_fuji --account development-1
+forge script script/DeployTokenTransferor.s.sol:DeployTokenTransferor --broadcast --rpc-url avalanche-fuji --account development-1
 
 # Send tokens
-forge script script/SendTokens.s.sol:SendTokens --broadcast --rpc-url avalanche_fuji --account development-1
+forge script script/SendTokens.s.sol:SendTokens --broadcast --rpc-url avalanche-fuji --account development-1
